1ca2b69cea2ce036287f7fcfc8d04dd11964f588
Arockiya Selva <arockiyaselva@gmail.com>
2020-10-15 10:41:38 +0400

    Operator input added

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -1,6 +1,20 @@
 print("Arockiya's Calculator")
 #  By default, all the inputs are considered strings
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
-result = float(num1)+float(num2)
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+op = input("Enter the operator:")
+
+if op == "+":
+    result = num1+num2
+elif op == "-":
+    result = num1-num2
+elif op == "*":
+    result = num1 * num2
+elif op == "/":
+    result = num1/num2
+else:
+    result = 0
+    print("Invalid operator")
+
+# result = float(num1)+float(num2)
 print("The result is: "+ str(result))
