82452679271c2602b11ab8196d1224e6e352862a
Arockiya Selva <arockiyaselva@gmail.com>
2020-10-15 11:27:07 +0400

    Palindrome

--- for loop.py
+++ for loop.py
@@ -1,4 +1,21 @@
-friends = ["Jim","Karen","Selva"]
+#friends = ["Jim","Karen","Selva"]
 
-for letter in range(3,10):
-    print(letter)
+#for letter in range(3,10):
+#    print(letter)
+# For straight range (range(100)) of 100 means 0-99
+# For reversed range (range(100,-1,-1)) of 100 means 100-0
+
+
+# Palimdrome
+
+word = input("Enter a word:")
+
+new_word=word[::-1]
+lw = len(word)
+
+if word.lower()==new_word.lower():
+    print("This is a Palindrome word")
+else:
+    print("This is not a Palindrome word")
+
+#print(lw)
