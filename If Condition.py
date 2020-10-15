f11df79a38a23f0638bd397d8c93e6593dc17dba
Arockiya Selva <arockiyaselva@gmail.com>
2020-10-15 10:33:34 +0400

    If elif

--- If Condition.py
+++ If Condition.py
@@ -1,6 +1,17 @@
 is_male = False
 
-if is_male:
+if not is_male:
     print("You are a male")
 else:
     print("You are not male")
+
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(7,8,9))
